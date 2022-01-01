44f29b60c1077c6a3fe713e16be4070db5b8974d
ilan1803 <cibot.ilan@gmail.com>
2022-01-01 17:46:25 +0200

    danger mines

--- src/main/java/Demineur.java
+++ src/main/java/Demineur.java
@@ -1,23 +1,61 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 
-/**
- *
- * @author cibot
- */
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Demineur {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-        int mines;
-        int colonne;
-        int ligne;
-        int level;
+
+        String level;
+        int ligne = 0;
+        int colonne = 0;
+        int mines = 0;
+        int x = 0;
+        int y = 0;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("choisie un niveau: \nd = dificile\nm = moyen\nf = facile");
+        level = sc.nextLine();
+
+        if (level == "d") {
+            colonne = ligne = 15;
+            mines = 100;
+        }
+        if (level == "m") {
+            colonne = ligne = 10;
+            mines = 30;
+        }
+        if (level == "f") {
+            colonne = ligne = 5;
+            mines = 10;
+        }
+
+        int[][] jeu = new int[colonne][ligne];
+
+        while (x < ligne) {
+            while (y < colonne) {
+                jeu[x][y] = 0;
+                y++;
+            }
+            x++;
+        }
+
+        while (mines > 0) {
+            for (x = 0, y = 0; x < ligne; y += Math.random() * 10, x++) {
+                for (int tmp = 0; tmp < colonne; tmp++) {
+                    if (mines == 0) {
+                        return;
+                    }
+                    jeu[x][y] = 1;
+                    mines--;
+                }
+                if (mines == 0) {
+                    return;
+                }
+            }
+        }
+        
+        
+
     }
-    
 }
